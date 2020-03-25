902d1f288fce860c08f4103e00cb0e92323d4ab6
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2020-03-25 19:40:13 +0000

    Strategies for Always Cooperate and Always Defect added

--- src/play/AlwaysDefectStrategy.java
+++ src/play/AlwaysDefectStrategy.java
@@ -1,8 +1,119 @@
 package play;
 
+import play.exception.InvalidStrategyException;
+
+import java.util.Iterator;
+
+import static play.StrategyCommonUtils.isATerminalGameNode;
+
 public class AlwaysDefectStrategy extends Strategy {
+
     @Override
     public void execute() throws InterruptedException {
 
+        // Waits until the Game Tree become known and available
+        while(!this.isTreeKnown()) {
+
+            System.err.println("Waiting for Game Tree to become Available...");
+
+            //noinspection BusyWait
+            Thread.sleep(1000);
+
+        }
+
+        // Infinite Loop
+        while(true) {
+
+            PlayStrategy myStrategy = this.getStrategyRequest();
+
+            // The Strategy chosen by me become NULL,
+            // what means (probably) that the Game
+            // was terminated by an outside event
+            if(myStrategy == null) {
+
+                // Breaks the Infinite Loop
+                break;
+
+            }
+
+            // Prints the Basic Information about this Strategy
+            System.out.println("Start playing with the Random Strategy...");
+
+            // My Play wasn't completed yet
+            boolean playComplete = false;
+
+            // While My Play isn't complete yet
+            while(! playComplete ) {
+
+                // Verify if the current Game Nodes for the Plays as both,
+                // Player #1 and Player #2, are Terminal Game Nodes or not
+                isATerminalGameNode
+                        (
+                                myStrategy,
+                                this.tree.getNodeByIndex(myStrategy.getFinalP1Node()),
+                                this.tree.getNodeByIndex(myStrategy.getFinalP2Node())
+                        );
+
+                // The Current Validation Set for the Available Moves
+                Iterator<Integer> currentValidationSetIterator = tree.getValidationSet().iterator();
+
+                // The Labels for All the Available Moves
+                Iterator<String> availableMovesLabels = myStrategy.keyIterator();
+
+                // Loop to check all the Entries of the Validation Set
+                // (i.e., a Decision Node of the Tree representing the current Game)
+                while(currentValidationSetIterator.hasNext()) {
+
+                    // Creates the array to keep the information for
+                    // the Probabilities for all the Available Moves
+                    double[] availableMovesProbabilities = new double[ currentValidationSetIterator.next() ];
+
+                    // Sets the Probability for the Cooperate (C) Move
+                    availableMovesProbabilities[0] = 0.0;
+
+                    // Sets the Probability for the Defect (D) Move
+                    availableMovesProbabilities[1] = 1.0;
+
+
+                    // Assigns all the current Available Probabilities to all the current Available Moves
+                    for(double availableMoveProbability : availableMovesProbabilities) {
+
+                        // The Strategy's Structure doesn't match the current Game
+                        if(!availableMovesLabels.hasNext()) {
+
+                            System.err.println("PANIC: Strategy's Structure doesn't match the current Game!!!");
+
+                            return;
+
+                        }
+
+                        // Assigns the current Available Probability to the current
+                        myStrategy.put( availableMovesLabels.next(), availableMoveProbability );
+
+                    }
+
+                }
+
+                try {
+
+                    // Sets and provides the final Strategy
+                    this.provideStrategy(myStrategy);
+
+                    // Sets My Play as completed, as long I'm finished playing
+                    playComplete = true;
+
+                }
+                catch (InvalidStrategyException invalidStrategyException) {
+
+                    System.err.println("Invalid Strategy: " + invalidStrategyException.getMessage());
+                    invalidStrategyException.printStackTrace(System.err);
+
+                }
+
+            }
+
+        }
+
     }
+
 }
