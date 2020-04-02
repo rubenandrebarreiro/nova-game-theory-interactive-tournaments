db4261e3348540afa4d8ab2f295b8ae13a451bb5
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2020-04-02 10:20:08 +0100

    Some minor changes added

--- src/play/OmegaTitForTatForPrisonerDilemmaTournamentGame1.java
+++ src/play/OmegaTitForTatForPrisonerDilemmaTournamentGame1.java
@@ -1,5 +1,28 @@
 package play;
 
+/**
+ *
+ * Computational Game Theory - 2019/2020
+ * Faculty of Sciences and Technology of
+ * New University of Lisbon (FCT NOVA)
+ *
+ * 1st Tournament - Iterated Prisoner's Dilemma,
+ * using NOVA GTI (Game Theory Interactive) Platform
+ *
+ * Omega Tit For Tat Strategy specially design for
+ * the specifications of the Game #1, which states:
+ * - 1) Total Number of 20 Iterations;
+ * - 2) Probability of 1.0 (i.e., 100%) of the Game
+ *      continue to the next Game Stage/Round;
+ *
+ * Authors:
+ * - Pedro Lamarao Pais (Student no. 48247)
+ *   - pgp@campus.fct.unl.pt
+ * - Ruben Andre Barreiro (Student no. 42648)
+ *   - r.barreiro@campus.fct.unl.pt
+ *
+ */
+
 public class OmegaTitForTatForPrisonerDilemmaTournamentGame1 extends Strategy {
     @Override
     public void execute() throws InterruptedException {
--- src/play/OmegaTitForTatForPrisonerDilemmaTournamentGame3.java
+++ src/play/OmegaTitForTatForPrisonerDilemmaTournamentGame3.java
@@ -1,5 +1,30 @@
 package play;
 
+/**
+ *
+ * Computational Game Theory - 2019/2020
+ * Faculty of Sciences and Technology of
+ * New University of Lisbon (FCT NOVA)
+ *
+ * 1st Tournament - Iterated Prisoner's Dilemma,
+ * using NOVA GTI (Game Theory Interactive) Platform
+ *
+ * Omega Tit For Tat Strategy specially design for
+ * the specifications of the Game #3, which states:
+ * - 1) Unknown Total Number of Iterations
+ *     (defined at the moment of the Game being initiated);
+ * - 2) Unknown Probability of the Game
+ *     (defined as less than 1.0, i.e., less than 100%)
+ *      continue to the next Game Stage/Round;
+ *
+ * Authors:
+ * - Pedro Lamarao Pais (Student no. 48247)
+ *   - pgp@campus.fct.unl.pt
+ * - Ruben Andre Barreiro (Student no. 42648)
+ *   - r.barreiro@campus.fct.unl.pt
+ *
+ */
+
 public class OmegaTitForTatForPrisonerDilemmaTournamentGame3 extends Strategy {
     @Override
     public void execute() throws InterruptedException {
