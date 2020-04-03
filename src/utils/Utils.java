a57569261fb5835fc95fca6a1efb629c9ce8da7d
Pedro Pais <pg.pais@campus.fct.unl.pt>
2020-04-03 09:49:46 +0100

    Linear programming

---
+++ src/utils/Utils.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Utils {
+
+    public static int[] nashEquilibrium(int[] u1, int[]u2){
+        int[] prob1 = new int[u1.length-1];
+        int[] prob2 = new int[u2.length-1];
+
+        
+
+        return new int[]{-1, -1};
+    }
+}
